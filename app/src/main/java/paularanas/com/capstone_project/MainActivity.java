2247586bfa2d952f938890be546c8dffeadae09f
Paul Aranas <Paul Aranas>
2016-05-30 07:21:27 -0700

    feat: Add toolbar

--- app/src/main/java/paularanas/com/capstone_project/MainActivity.java
+++ app/src/main/java/paularanas/com/capstone_project/MainActivity.java
@@ -2,12 +2,17 @@ package paularanas.com.capstone_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 }
