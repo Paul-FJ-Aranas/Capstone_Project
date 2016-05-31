5576b29837e6ba550ffdd7a19b354f6853ae6c35
Paul Aranas <paul.aranas@ymail.com>
2016-05-31 10:43:17 -0700

    feat: Add details activity and fragment, move classes and fragment into package folder, add Coordinator Layout and RecyclerView to main fragment xml

--- app/src/main/java/MainGridFragment.java
+++
@@ -1,9 +0,0 @@
-import android.app.Fragment;
-
-/**
- * Created by Paul Aranas on 5/30/2016.
- */
-public class MainGridFragment extends Fragment {
-
-
-}
---
+++ app/src/main/java/paularanas/com/capstone_project/GardenDetailsFragment.java
@@ -0,0 +1,20 @@
+package paularanas.com.capstone_project;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Paul Aranas on 5/31/2016.
+ */
+public class GardenDetailsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
