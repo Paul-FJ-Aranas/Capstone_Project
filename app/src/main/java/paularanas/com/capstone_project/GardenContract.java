304cbf1947884671de8751aad4273a12a68225ea
Paul Aranas <paul.aranas@ymail.com>
2016-06-03 11:58:09 -0700

    feat: Add GardenDatabaseHelper and GardenContract classes

---
+++ app/src/main/java/paularanas/com/capstone_project/GardenContract.java
@@ -0,0 +1,60 @@
+package paularanas.com.capstone_project;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by Paul Aranas on 6/3/2016.
+ */
+public class GardenContract {
+
+    private GardenContract() {
+    }
+
+    public static final String DATABASE_NAME = "garden_database";
+    public static final int DATABASE_VERSION = 1;
+    public static final String AUTHORITY = "com.paularanas.capstone_project.gardenprovider";
+    public static final Uri URI_BASE = new Uri.Builder()
+            .scheme(ContentResolver.SCHEME_CONTENT)
+            .authority(AUTHORITY).build();
+    public static final Uri URI_GARDENS = URI_BASE.buildUpon()
+            .appendPath(GardenTable.TABLE_NAME).build();
+    public static final String TYPE_FAVS = ContentResolver.CURSOR_DIR_BASE_TYPE
+            + "/vnd." + AUTHORITY + GardenTable.TABLE_NAME;
+    private static final String COMMA = ",";
+    private static final String TEXT = " TEXT";
+    private static final String REAL = " REAL";
+    private static final String INTEGER = " INTEGER";
+    private static final String NOT_NULL = " NOT NULL";
+
+
+    public static abstract class GardenTable implements BaseColumns {
+        public static final String TABLE_NAME = "gardens";
+        public static final String _ID = "_id";
+        public static final String PHOTO = "photoPath";
+        public static final String TITLE = "title";
+        public static final String CREATOR = "creator";
+        public static final String THUMBNAIL_PATH = "thumbnail";
+        public static final String BODY = "body";
+        ;
+
+        public static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS " +
+                TABLE_NAME + "(" + _ID + INTEGER + " PRIMARY KEY AUTOINCREMENT " + COMMA +
+                PHOTO + TEXT + " UNIQUE " + COMMA +
+                TITLE + TEXT + COMMA +
+                CREATOR + TEXT + COMMA +
+                THUMBNAIL_PATH + TEXT + COMMA +
+                BODY + TEXT + ")";
+
+        public static final String[] PROJECTION_ALL =
+                {_ID, PHOTO, TITLE,
+                        CREATOR, THUMBNAIL_PATH,
+                        BODY
+                };
+
+        public static final String DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
+
+
+    }
+}
---
+++ app/src/main/java/paularanas/com/capstone_project/GardenDatabaseHelper.java
@@ -0,0 +1,25 @@
+package paularanas.com.capstone_project;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Paul Aranas on 6/3/2016.
+ */
+public class GardenDatabaseHelper extends SQLiteOpenHelper {
+
+    public GardenDatabaseHelper(Context context) {
+        super(context, GardenContract.DATABASE_NAME, null, GardenContract.DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
