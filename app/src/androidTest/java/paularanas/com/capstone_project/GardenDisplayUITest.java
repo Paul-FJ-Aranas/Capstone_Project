0cb65dab8fc7c77a629727facd551717d646d18f
Paul Aranas <paul.aranas@ymail.com>
2016-07-14 05:00:15 -0700

    feat: Add code to Espresso UI test

--- app/src/androidTest/java/paularanas/com/capstone_project/GardenDisplayUITest.java
+++ app/src/androidTest/java/paularanas/com/capstone_project/GardenDisplayUITest.java
@@ -1,5 +1,6 @@
 package paularanas.com.capstone_project;
 
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
@@ -10,7 +11,9 @@ import org.junit.runners.JUnit4;
 import paularanas.com.capstone_project.ui.MainActivity;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.swipeLeft;
+import static android.support.test.espresso.action.ViewActions.swipeRight;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -33,7 +36,18 @@ public class GardenDisplayUITest {
         onView(withId(R.id.main_pager)).perform(swipeLeft());
         onView(withText("Welcome"));
         onView(withId(R.id.main_pager)).perform(swipeLeft()).check(matches(hasDescendant(withId(R.id.start_button))));
-    }
+        onView(withId(R.id.main_pager)).perform(swipeLeft());
+        onView(withId(R.id.main_pager)).perform(swipeRight());
+        onView(withId(R.id.main_pager)).perform(swipeRight());
+        onView(withId(R.id.main_pager)).perform(swipeRight());
 
+    }
+    @Test
+    public void DetailsGardenDisplayUITest() {
+        onView(withId(R.id.recycler_view))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.pager)).perform(swipeLeft());
+        onView(withId(R.id.pager)).perform(swipeLeft());
+    }
 
 }
